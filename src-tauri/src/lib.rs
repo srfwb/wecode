89ce51a155861de2ac18c93f713af5750a68f21c
srfwb <264158739+srfwb@users.noreply.github.com>
2026-04-22 21:03:25 +0200

    chore(deps): add tauri-plugin-dialog for native folder picker

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_dialog::init())
         .manage(VfsState::default())
         .manage(RecentWritesState::default())
         .manage(WatcherState::default())
